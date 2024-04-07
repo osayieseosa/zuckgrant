64ea3601149102d0082282dd60fa25fd6e6e7196
Osayi Eseosa <osayivictoryeseosa@gmail.com>
2024-04-07 21:28:21 +0100

    bug fixes

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -86,7 +86,7 @@ const Drawer = () => {
                 <Link to="/winners">Lottery Winners</Link>
               </li>
               <li>
-                <Link to="/compform">compliance form</Link>
+                <Link to="/compform">Claim Application</Link>
               </li>
               <li>
                 <Link to="/privacy">Privacy Policy</Link>
@@ -129,7 +129,7 @@ const Drawer = () => {
             <Link to="/winners">Lottery Winners</Link>
           </li>
           <li>
-            <Link to="/compform">Compliance Form</Link>
+            <Link to="/compform">Claim Application</Link>
           </li>
           <li>
             <Link to="/privacy">Privacy Policy</Link>
