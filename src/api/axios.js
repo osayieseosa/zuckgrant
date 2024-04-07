6cb0b6c7225d059f7e7328ec8f2e5f5ebec1c5b5
Osayi Eseosa <osayivictoryeseosa@gmail.com>
2024-04-07 21:06:40 +0100

    email

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// "https://localhost:3130"
 
-// "https://zuckgrantapi.onrender.com"
+
+// "https://localhost:3130"
 
 export default axios.create({
     baseURL: "https://zuckgrantapi.onrender.com"
