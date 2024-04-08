d82104bb45d23b5f79b76bc765e7a6fc9367feea
Osayi Eseosa <osayivictoryeseosa@gmail.com>
2024-04-08 02:47:01 +0100

    Update NewUser.jsx

--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -137,14 +137,14 @@ const NewUser = () => {
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Enter Lucky Number</span>
+                  <span className="label-text">Enter Winning Number</span>
                 </label>
                 <input
                   type="text"
                   value={luckyNumber}
                   onChange={(e) => setLuckyNumber(e.target.value)}
                   required
-                  placeholder="Full Name"
+                  placeholder="winning number"
                   className="input input-bordered input-primary w-full  max-w-xs"
                 />
               </div>
